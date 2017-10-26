47fea043321134eb8d345c95d82606087ccc7505
FaizeChan <obey.faize@gmail.com>
2017-10-26 16:06:14 -0400

    finished button and setted the date

--- Bookshelf/Models/Book.cs
+++ Bookshelf/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -15,11 +16,15 @@ namespace Bookshelf.Models
         public int ID { get; set; }
         [MinLength(2),MaxLength(50)]
         public string Title { get; set; }
+        [Display(Name = "Published Date")]
         public DateTime PublishedDate { get; set; }
         [MaxLength(500)]
         public string Description { get; set; }
+        //[Required]
         public string TSBN { get; set; }
 
-        
+        [ForeignKey("Category"), Display(Name ="Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
